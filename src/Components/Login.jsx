b8378f4d7534971fcb83d8b3373cb7a1b4c7dba9
Mukul Verma <mukulv072@gmail.com>
2025-09-04 18:11:03 +0530

    made login & signup logic with localStorage

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,39 +4,26 @@ import { useNavigate } from 'react-router-dom'
 const Login = () => {
   const API = "http://localhost:3000/users"
 
-  const [name, setName] = useState("")
+  const [userName, setUserName] = useState("")
   const [pass, setPass] = useState('');
   const [pending, setPending] = useState(false)
   const navigate = useNavigate();
+  
+  const user = JSON.parse(localStorage.getItem(`user_${userName}`));
 
-  const send = async () => {
-    if (name && pass) {
-      try {
-        const response = await fetch(API)
-        const data = await response.json()
-        const matched = data.find((item) => item.name === name && item.password === pass)
-        if (matched) {
-          setPending(true)
-          localStorage.setItem("name", name)
-          navigate('/')
-          location.reload()
-        }
-        else {
-          setPending(false)
-        }
-      }
-      catch (err) {
-        return
-      }
+  const send = () => {
+    if (userName === user.username && pass === user.password) {
+      navigate('/');
     }
   }
+
 
   return (
     <div className="box">
       <div className="box-1">
         <h1 className='h1'>Login</h1>
         <form className='form'>
-          <input type="text" placeholder='Enter email or username' className='Input' onChange={(e) => setName(e.target.value)} />
+          <input type="text" placeholder='Enter username' className='Input' onChange={(e) => setUserName(e.target.value)} />
           <input type="password" placeholder='Enter password' className='Input' onChange={(e) => setPass(e.target.value)} />
           <button type='button' className='loginBtn' disabled={pending} onClick={send}>Submit</button>
           <div className="div">
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const obj = {
+    name: "",
+    email: "",
+    username: "",
+    password: ""
+}
+
+const reducer = (data, action) => {
+    return { ...data, [action.type]: action.val }
+}
 
 const Signup = () => {
+    const [state, dispatch] = useReducer(reducer, obj);
+    const navigate = useNavigate();
+
+    //Name validation
+
+    // const nameVal = () => {
+    //     if (state.name === "") {
+    //         console.log("name cant be empty");
+    //     }
+    // }
+
+    const send = () => {
+        console.log(state);
+
+        if (state.name && state.email && state.username && state.password !== "") {
+            const unqKey = `user_${state.username}`
+            localStorage.setItem(unqKey, JSON.stringify(state))
+            navigate('/login')
+        }
+    }
     return (
         <>
             <div className="box">
                 <div className="box-1">
                     <h1 className='h1'>SignUp</h1>
                     <form className='form'>
-                        <input type="text" placeholder='Enter Name' className='Input' />
-                        <input type="text" placeholder='Enter Email' className='Input' />
-                        <input type="text" placeholder='Enter Username' className='Input' />
-                        <input type="password" placeholder='Enter Password' className='Input' />
-                        <button type='button' className='loginBtn'>Submit</button>
+                        <input type="text" placeholder='Enter Name' className='Input' onChange={(e) => { dispatch({ val: e.target.value, type: "name" }) }} />
+                        <input type="text" placeholder='Enter Email' className='Input' onChange={(e) => dispatch({ val: e.target.value, type: "email" })} />
+                        <input type="text" placeholder='Enter Username' className='Input' onChange={(e) => dispatch({ val: e.target.value, type: "username" })} />
+                        <input type="password" placeholder='Enter Password' className='Input' onChange={(e) => dispatch({ val: e.target.value, type: "password" })} />
+                        <button type='button' className='loginBtn' onClick={send}>Submit</button>
                         <div className="div">
                             <span>Don't have account?</span> <a href="" className='text-blue-700 font-semibold'>Sign Up</a>
                         </div>
